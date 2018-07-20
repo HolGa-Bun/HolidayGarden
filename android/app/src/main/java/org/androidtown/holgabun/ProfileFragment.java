8cbf64cc19acfa3431fed17f746ee45760bb831a
jaeryo2357@naver.com <Jaeryo@DESKTOP-EJJGE7H.localdomain>
2018-07-20 13:34:28 +0900

    아직

--- android/app/src/main/java/org/androidtown/holgabun/ProfileFragment.java
+++ android/app/src/main/java/org/androidtown/holgabun/ProfileFragment.java
@@ -119,6 +119,7 @@ public class ProfileFragment extends Fragment{
     String feed_id;
     ArrayList<Bitmap> please;
     String url;
+    TextView t;
 
     ImageView img;
 <<<<<<< HEAD
@@ -216,12 +217,16 @@ public class ProfileFragment extends Fragment{
 
         listView=(ListView)view.findViewById(R.id.myfeed);
 
+        t=(TextView)view.findViewById(R.id.nickname_profile);
+
+
 
 
         DbOpenHelper h=new DbOpenHelper(getActivity());
 
         h.open();
 
+        t.setText(h.returnId());
 
         feed_id=h.returnId();
 
